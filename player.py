5b30790ab80c47a40a521baffdf6bc956b905971
Anirudh Jayakumar <anirudh.jayakumar@outlook.com>
2018-10-26 23:54:49 +0100

    Player framework

---
+++ player.py
@@ -0,0 +1,30 @@
+class Player:
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def reward(self):
+        pass
+
+class QLearningPlayer(Player):
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def reward(self):
+        pass
+
+class RandomPlayer(Player):
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def reward(self):
+        pass
+
