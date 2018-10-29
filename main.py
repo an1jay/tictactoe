fa8423dbfaecc0250da3790c84e4a3c53bd85442
z0ldam <44512805+z0ldam@users.noreply.github.com>
2018-10-29 20:22:18 +0000

    Basic AI Player

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import os
 import numpy as np
 import time
-from generate import generateExamples
+#from generate import generateExamples
 from game import TicTacToe
+from player import BasicAIPlayer
 from player import HumanPlayer
-from player import SophisticatedRandomPlayer
 from player import RandomPlayer
+from player import SophisticatedRandomPlayer
+from progress import progress
 from train import Evaluator
 
 if __name__ == "__main__":
-    filename ='example_100k.pkl'
+    filename ='example_10k.pkl'
     if not os.path.isfile(filename):
         filename = generateExamples(int(1e5))
-    e = Evaluator()
-    print(e.evaluate(np.zeros((18))))
-
-    e.load_data(filename)
-    e.fit()
-    print(e.evaluate(np.zeros((18))))
-
+    e = Evaluator((27,'relu',27,'relu',27,'tanh',1),loss='mean_squared_error',optimizer='adam')
+    f = Evaluator((27,'tanh',27,'tanh',27,'tanh',1),loss='mean_squared_error',optimizer='adam')
+    
+    for ev in [e,f]:
+        if not os.path.isfile(ev.filename()):
+            ev.load_data(filename)
+            ev.fit()
+            ev.save_model()
+        else:
+            ev.load_model(ev.filename())
+     
+    scores = np.zeros((2,3)) 
+    players = (BasicAIPlayer(e), SophisticatedRandomPlayer())
+    for i in range(1000):
+        if i%20 == 0:
+            progress(i , 1000, status="Playing Games")
+        x = np.random.randint(0,2)
+        p1 = players[x]
+        p2 = players[1-x]
+        t = TicTacToe(p1, p2, verbose = False)
+        winner = t.play()
+        scores[x][1+winner] += 1
+        
+    print(scores)
+        
--- train.py
+++ train.py
@@ -7,34 +7,43 @@ import numpy as np
 
 
 class Evaluator:
-    def __init__(self):
+    def __init__(self, architecture, loss, optimizer):
+        self.architecture = architecture
+        
         self.model = Sequential()
-        self.model.add(Dense(27, input_shape=(18,)))
-        self.model.add(Activation("tanh"))
-        self.model.add(Dense(27))
-        self.model.add(Activation("tanh"))
-        self.model.add(Dense(27))
-        self.model.add(Activation("tanh"))
-        self.model.add(Dense(1))
-        self.model.compile(loss="mean_squared_error", optimizer="adam")
+        self.model.add(Dense(architecture[0], input_shape=(18,)))
+        
+        for layer in architecture:
+            if isinstance(layer, str):
+                self.model.add(Activation(layer))
+            else:
+                self.model.add(Dense(layer))
+
+        self.model.compile(loss=loss, optimizer=optimizer)
 
     def load_data(self, filename):
         with open(filename, "rb") as f:
             x, y = pickle.load(f)
         self.x_train = np.concatenate(
-            list(map(lambda a: a.reshape((1, 18)), x)), axis=0
-        )
+            list(map(lambda a: a.reshape((1, 18)), x)), axis=0)
         self.y_train = np.array(y)
 
     def load_model(self, filename):
         self.model = load_model(filename)
 
-    def fit(self):
+    def fit(self, epochs=3, batch_size=32):
+        self.model.fit(self.x_train, self.y_train, epochs=epochs, batch_size=batch_size)
 
-        self.model.fit(self.x_train, self.y_train, epochs=3)
-
-    def save_model(self, model_name):
+    def save_model(self):
+        model_name = self.filename()
         self.model.save(model_name)
-
+        return model_name
+        
     def evaluate(self, board):
         return self.model.predict(board.reshape((1, 18)))[0][0]
+
+    def name(self):
+        return '-'.join([str(l) for l in self.architecture])
+        
+    def filename(self):
+        return self.name() + '.h5'
